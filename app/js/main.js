150ac2bad3141f55f76497a030dd4696b74982e8
unknown <pedro.henrickcs@gmail.com>
2019-11-11 23:14:20 -0300

    include jquery and slick and mobile

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,14 @@
-console.log('This is the Main.js file. It should be the 3rd and final file');
+$(() => {
+    console.log('slick');
+
+    if ($(window).width() <= 768) {
+        console.log('entrou no if');
+        
+        $('.detalhes').slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        });
+    }
+    
+});
