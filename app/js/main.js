82497d4a15483017506e432a8c80fe9d47a260bb
unknown <pedro.henrickcs@gmail.com>
2019-11-13 00:50:45 -0300

    finishing page and responsive

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
 $(() => {
-    console.log('slick');
+    console.log('slick V1.0.0');
 
     if ($(window).width() <= 768) {
         console.log('entrou no if');
@@ -7,7 +7,25 @@ $(() => {
         $('.detalhes').slick({
             infinite: true,
             slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToScroll: 1,
+            arrows: false,
+            dots: false
+        });
+
+        $('#option .option').slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: false
+        });
+
+        $('#why .box-card').slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: false
         });
     }
     
